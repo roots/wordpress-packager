f32946555903cb4bdb94ea5e11f6defb7f549f41
Austin Pray <austin@austinpray.com>
2019-05-30 23:50:17 -0500

    use url parser

--- src/WordPressPackager/ReleaseSources/WPDotOrgHTML.php
+++ src/WordPressPackager/ReleaseSources/WPDotOrgHTML.php
@@ -9,10 +9,12 @@ use DOMElement;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use InvalidArgumentException;
+use League\Uri\Components\HierarchicalPath as Path;
+use League\Uri\Components\Host;
+use League\Uri\Http;
 use Roots\WordPressPackager\WordPressPackage;
 use Roots\WordPressPackager\WordPressPackageRepository;
 use Symfony\Component\DomCrawler\Crawler;
-use Symfony\Component\DomCrawler\Link;
 
 class WPDotOrgHTML implements SourceInterface
 {
@@ -30,35 +32,27 @@ class WPDotOrgHTML implements SourceInterface
         $this->html = $html;
     }
 
+    private static function isValidBasename(string $basename)
+    {
+        return (
+            // whitelist
+            Str::startsWith($basename, 'wordpress-') &&
+            Str::endsWith($basename, ['.zip']) &&
+            // blacklist
+            !Str::startsWith($basename, 'wordpress-mu') &&
+            !Str::endsWith($basename, ['-IIS.zip'])
+        );
+    }
+
     public static function isValidReleaseURL(string $url): bool
     {
-        $httpUrl = Collection::make(parse_url($url));
-
-        if ($httpUrl->get('scheme') !== 'https') {
-            return false;
-        }
-        if ($httpUrl->get('host') !== 'wordpress.org') {
-            return false;
-        }
-
+        $httpUrl = Http::createFromString($url);
+        $host = new Host($httpUrl->getHost());
         $basename = self::getBasename($httpUrl);
+        $isHttps = $httpUrl->getScheme() === 'https';
+        $wpOrgDomain = $host->getRegistrableDomain() === 'wordpress.org';
 
-        if ($basename === '') {
-            return false;
-        }
-
-        // whitelist
-        if (!Str::startsWith($basename, 'wordpress-')) {
-            return false;
-        }
-        if (!Str::endsWith($basename, ['.zip'])) {
-            return false;
-        }
-        // blacklist
-        if (Str::startsWith($basename, 'wordpress-mu')) {
-            return false;
-        }
-        if (Str::endsWith($basename, ['-IIS.zip'])) {
+        if (!$basename || !$isHttps || !$wpOrgDomain || !self::isValidBasename($basename)) {
             return false;
         }
 
@@ -67,9 +61,9 @@ class WPDotOrgHTML implements SourceInterface
         return self::isVersion($version);
     }
 
-    protected static function getBasename(Collection $urlParts): string
+    protected static function getBasename(Http $url): string
     {
-        return Collection::make(explode('/', $urlParts->get('path', '')))->last();
+        return (new Path($url->getPath()))->getBasename();
     }
 
     protected static function getVersionFromBasename(string $basename): ?string
@@ -81,7 +75,7 @@ class WPDotOrgHTML implements SourceInterface
 
     protected static function getVersionFromURL(string $url): ?string
     {
-        $httpUrl = Collection::make(parse_url($url));
+        $httpUrl = Http::createFromString($url);
         return self::getVersionFromBasename(self::getBasename($httpUrl));
     }
 
@@ -92,6 +86,23 @@ class WPDotOrgHTML implements SourceInterface
         } catch (\UnexpectedValueException $_) {
             return false;
         }
+    }
+
+    protected static function packageFromUrl(string $releaseUrl): WordPressPackage
+    {
+        // TODO: don't hardcode package org
+        $org = 'roots';
+
+        $name = "$org/wordpress-dotorg";
+        /** @var string $version Version is guaranteed because of `isValidReleaseURL` above. */
+        $version = self::getVersionFromURL($releaseUrl);
+
+        $package = new WordPressPackage($name, $version);
+
+        $package->setDistType('zip');
+        $package->setDistUrl($releaseUrl);
+
+        return $package;
     }
 
     public function getRepo(): WordPressPackageRepository
@@ -117,20 +128,8 @@ class WPDotOrgHTML implements SourceInterface
                 })
                 ->filter()
                 ->unique()
-                ->map(function (string $releaseUrl): WordPressPackage {
-                    // TODO: don't hardcode package org
-                    $org = 'roots';
-
-                    $name = "$org/wordpress-dotorg";
-                    /** @var string $version Version is guaranteed because of `isValidReleaseURL` above. */
-                    $version = self::getVersionFromURL($releaseUrl);
-
-                    $package = new WordPressPackage($name, $version);
-
-                    $package->setDistType('zip');
-                    $package->setDistUrl($releaseUrl);
-
-                    return $package;
+                ->map(function ($url) {
+                    return self::packageFromUrl($url);
                 })
                 ->toArray()
         );
