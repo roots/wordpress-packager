64bd3cb92ba9a4115f12e78e05a1400138c36719
Léo Colombaro <git@colombaro.fr>
2022-02-13 19:14:55 +0100

    Test json output completeness

--- tests/Package/PacakgeTest.php
+++ tests/Package/PacakgeTest.php
@@ -24,12 +24,12 @@ class PacakgeTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        
-        $builder = new Package('roots/wordpress');
-        
-        $this->pkgA = $builder->clone()->withVersion('5.2.1');
-        $this->pkgB = $builder->clone()->withVersion('5.2.1');
-        $this->pkgC = $builder->clone()->withVersion('5.0');
+
+        $this->builder = new Package('roots/wordpress');
+
+        $this->pkgA = $this->builder->clone()->withVersion('5.2.1');
+        $this->pkgB = $this->builder->clone()->withVersion('5.2.1');
+        $this->pkgC = $this->builder->clone()->withVersion('5.0');
 
         $this->pkgAAlpha = (new Package('roots/wordpress-a'))->withVersion('5.2.1');
         $this->pkgBAlpha = (new Package('roots/wordpress-b'))->withVersion('5.2.1');
@@ -59,6 +59,9 @@ class PacakgeTest extends TestCase
 
     public function testJsonSerialize()
     {
+        $this->pkgA->withRequires();
+        $this->pkgA->withProvides();
+
         $json = new JsonFile($this->jsonFile);
         $serialized = $this->pkgA->jsonSerialize();
         $json->write($serialized);
@@ -67,6 +70,8 @@ class PacakgeTest extends TestCase
         file_put_contents($this->jsonFile, json_encode($this->pkgA));
         $json = new JsonFile($this->jsonFile);
         $this->assertTrue($json->validateSchema());
+
+        $this->assertJsonFileEqualsJsonFile($this->jsonFile, __DIR__ . '/../resources/composer.json');
     }
 
     public function testGreaterThanOrEqualTo()
@@ -87,9 +92,9 @@ class PacakgeTest extends TestCase
             });
             return $link->getConstraint();
         };
-        
+
         $builder = new Package('roots/wordpress');
-        
+
         $phpNew1 = $getPhpPackage($builder->clone()->withVersion('5.2.1'));
         $phpNew2 = $getPhpPackage($builder->clone()->withVersion('5.2'));
         $phpNew3 = $getPhpPackage($builder->clone()->withVersion('5.2-beta1'));
