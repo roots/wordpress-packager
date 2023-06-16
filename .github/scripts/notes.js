159860aea0ec7f15cb1e734033f592133941bbff
Léo Colombaro <git@colombaro.fr>
2023-06-17 00:42:28 +0200

    Refactor GitHub Actions workflows
    ...to actually make it runnable

--- .github/scripts/notes.js
+++ .github/scripts/notes.js
@@ -1,26 +1,46 @@
-module.exports = async ({ core, fetch }) => {
-  const { VERSION } = process.env
-  const slug = `version-${VERSION.replaceAll('.', '-')}`
+module.exports = async ({ github, core, fetch }) => {
+  const { TAGS_MATRIX, META_PACKAGE } = process.env
+  const tags = JSON.parse(TAGS_MATRIX)
 
-  try {
-    const res = await fetch('https://wordpress.org/documentation/wp-json/wp/v2/wordpress-versions?per_page=50')
-    const data = await res.json()
+  return Promise.allSettled(
+    tags.map((tag_name) =>
+      core.group(tag_name, async (tag_name) => {
+        const slug = `version-${tag_name.replaceAll('.', '-')}`
 
-    const release = data.find((tag) => tag.slug === slug)
-    if (!release) {
-      throw Error('Release not found')
-    }
+        try {
+          const res = await fetch(
+            'https://wordpress.org/documentation/wp-json/wp/v2/wordpress-versions?per_page=50'
+          )
+          const data = await res.json()
 
-    const { link } = release
-    const body = release.content?.rendered?.split('<h2', 4)[2]
-    if (!body) {
-      throw Error('Release body is empty or unexpected')
-    }
+          const release = data.find((tag) => tag.slug === slug)
+          if (!release) {
+            throw Error('Release not found')
+          }
 
-    return `_Sourced from [WordPress.org Documentation](${link})._\n\n<h2${body}`
-  } catch (e) {
-    core.warning(e)
+          const { link } = release
+          const body = release.content?.rendered?.split('<h2', 4)[2]
+          if (!body) {
+            throw Error('Release body is empty or unexpected')
+          }
 
-    return `_Version notes available on [WordPress.org Documentation](https://wordpress.org/documentation/wordpress-version/${slug}/)._`
-  }
+          const body = `_Sourced from [WordPress.org Documentation](${link})._\n\n<h2${body}`
+        } catch (e) {
+          core.error(e)
+
+          const body = `_Version notes available on [WordPress.org Documentation](https://wordpress.org/documentation/wordpress-version/${slug}/)._`
+        }
+
+        core.info('Publishing')
+        return github.rest.repos.createRelease({
+          owner: context.repo.owner,
+          repo: META_PACKAGE.substring(META_PACKAGE.indexOf('/') + 1),
+          tag_name,
+          body,
+          name: `Version ${tag_name}`,
+          make_latest: 'legacy',
+        })
+      })
+    )
+  )
 }
