5d8610548fcc0c390d74b85f3dc69bae1bb1f300
Léo Colombaro <git@colombaro.fr>
2023-08-09 00:41:49 +0200

    Tweak note script and ensure proper output
    Drop promise argument to use upper layer variable

--- .github/scripts/notes.js
+++ .github/scripts/notes.js
@@ -4,10 +4,11 @@ module.exports = async ({ github, context, core, fetch }) => {
 
   return Promise.allSettled(
     tags.map((tag_name) =>
-      core.group(tag_name, async (tag_name) => {
+      core.group(tag_name, async () => {
         const slug = `version-${tag_name.replaceAll('.', '-')}`
         let body = ''
 
+        core.info('Retrieve content')
         try {
           const res = await fetch(
             'https://wordpress.org/documentation/wp-json/wp/v2/wordpress-versions?per_page=50'
@@ -25,10 +26,8 @@ module.exports = async ({ github, context, core, fetch }) => {
             throw Error('Release body is empty or unexpected')
           }
 
-          core.info(tag_name)
           body = `_Sourced from [WordPress.org Documentation](${link})._\n\n<h2${body}`
         } catch (e) {
-          core.info(tag_name)
           core.error(e)
 
           body = `_Version notes available on [WordPress.org Documentation](https://wordpress.org/documentation/wordpress-version/${slug}/)._`
